3767e08743464e3f432b2ed2d6af26753267c20f
求其5 <122260316@qq.com>
2018-04-05 14:01:43 +0800

    string

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,9 +1,12 @@
+//this is branch dev
+
 package hello;
 
 import java.io.Console;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.concurrent.atomic.AtomicLong;
+import java.security.MessageDigest;
 
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    // private final String token="weixin";
+    private final String token="weixin";
 
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="signature") String signature , @RequestParam(value="timestamp") String timestamp , @RequestParam(value="nonce") String nonce , @RequestParam(value="echostr") String echostr) {
@@ -22,9 +25,15 @@ public class GreetingController {
         System.out.println(nonce);
         System.out.println(echostr);
         
-        // String tempArray[]={token,timestamp,nonce};
-        // Arrays.sort(tempArray);
-        // DigestUtils.s
+        String tempArray[]={token,timestamp,nonce};
+        Arrays.sort(tempArray);
+        int len=tempArray.length;
+        System.out.println(len);
+
+
+        // MessageDigest sha1=MessageDigest.getInstance(SHA-1);
+        // byte[] strbyte=signature.
+
                 
         return echostr;
     }
